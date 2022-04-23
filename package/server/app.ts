16d5a15e2942f15951cb54830fab3a2b0e41a216
shadowings-zy <shadowingszy@outlook.com>
2022-04-23 21:06:02 +0800

    feat: 修改主页

--- package/server/app.ts
+++ package/server/app.ts
@@ -17,8 +17,13 @@ const main = async () => {
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
 
+  /**
+   * 解释一下背景，原来的前端页面挂在了 linklearner.com/datawhale-homepage 下，
+   * 后来改成了 linklearner.com 下，所以这里放了两份静态资源。
+   */
   app.use('/datawhale-homepage', express.static(config.staticFilePath));
-  app.use('/', express.static(config.homepageFilePath));
+  app.use('/', express.static(config.staticFilePath));
+
   // 使用getRouter是为了防止在建立dbconnection之前初始化entity
   app.use('/api', getRouter());
   app.listen(port, () => {
